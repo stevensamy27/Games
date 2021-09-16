5c3367c56ecc94c8f19e8798d6e28508a9ed0756
Steven Samy <stevensivo27@gmail.com>
2021-09-16 18:47:25 +0200

    basics game elements

--- pong.py
+++ pong.py
@@ -1,10 +1,37 @@
 import turtle
 
-wn = turtle.screen()
+wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
-wn.setup(width=800, heigt=600)
+wn.setup(width=800, height=600)
 wn.tracer(0)
+
+
+#Paddle A 
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Ball
+Ball = turtle.Turtle()
+Ball.speed(0)
+Ball.shape("square")
+Ball.color("white")
+Ball.penup()
+Ball.goto(0, 0)
 
 
 #Main Game Loop
