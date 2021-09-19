e8e2d07fa7af656bc776994362708744ece34363
Steven Samy <stevensivo27@gmail.com>
2021-09-19 21:51:01 +0200

    add some functions

--- pong.py
+++ pong.py
@@ -9,7 +9,7 @@ wn.tracer(0)
 
 #Paddle A 
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(100)
 paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
@@ -32,24 +32,41 @@ Ball.shape("square")
 Ball.color("white")
 Ball.penup()
 Ball.goto(0, 0)
-
+Ball.dx = 2 
+Ball.dy = 2
 #Function
 def paddle_a_up():
     y = paddle_a.ycor()
-    y +=20
-    paddle_a.set(y)
+    y += 20
+    paddle_a.sety(y)
 
 
 def paddle_a_down():
     y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y +=20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
     y -=20
-    paddle_a.set(y)
+    paddle_b.sety(y)
 
 #Keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up,"w")
 wn.onkeypress(paddle_a_down,"s")
-
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
 #Main Game Loop
 while True:
     wn.update()
+
+    #Move The Ball
+    # Ball.setx(Ball.xcore() + Ball.dx)
+    # Ball.sety(Ball.ycore() + Ball.dy)
